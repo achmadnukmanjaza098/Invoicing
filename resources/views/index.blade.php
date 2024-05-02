d791fa020d16dcde361cd9a77d32fdee86e57aea
Rizky Aditiyan <rizkyaditiyan87@gmail.com>
2024-05-02 22:14:19 +0700

    tambahan ui dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,12 +22,42 @@
                         <div class="col-3">
                             <div class="text-primary p-3">
                                 <h5 class="text-primary">Welcome Back !</h5>
-                                <p>Hallo Selamat {{ $timeOfDay }} {{ ucfirst(Auth::user()->name) }}</p>
+                                <p>Hallo, Selamat {{ $timeOfDay }} <b>{{ ucfirst(Auth::user()->name) }}</b></p>
                             </div>
                         </div>
                         <div class="col-9 text-sm-end">
                             <img src="{{ asset('/assets/images/profile-img.png') }}" alt="" class="img-fluid"
                                 style="height:100px;">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body pt-4">
+                    <div>
+                        <div class="row">
+                            <div class="col-7">
+                            </div>
+                            <div class="col-3">
+                                <select id="month" name="month" class="form-control select2" style="width:250px">
+                                    <option value="01">Januari</option>
+                                    <option value="02">Februari</option>
+                                    <option value="03">Maret</option>
+                                    <option value="04">April</option>
+                                    <option value="05">Mei</option>
+                                    <option value="06">Juni</option>
+                                    <option value="07">Juli</option>
+                                    <option value="08">Agustus</option>
+                                    <option value="09">September</option>
+                                    <option value="10">Oktober</option>
+                                    <option value="11">November</option>
+                                    <option value="12">Desember</option>
+                                </select>
+                            </div>
+                            <div class="col-2">
+                                <select id="year" name="year" class="form-control select2" style="width:120px">
+                                    <option value="2024">2024</option>
+                                    <option value="2025">2025</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,9 +68,10 @@
                                 <div class="card-body" style="background-color:#dbebfa">
                                     <div class="d-flex">
                                         <div class="flex-grow-1">
-                                            <h4 class="mb-2 font-size-20">{{ $order }}</h4>
+                                            <h1 style="font-size:26px">{{ $order }}</h1>
                                             <p class="text-muted mb-0 text-truncate"><b>Order</b></p>
-                                            <p class="text-muted fw-medium font-size-12">{{ $percentageOrder }}</p>
+                                            <p class="text-muted"><span class="text-success me-2"> 12% <i class="mdi mdi-arrow-up"></i> </span> dari Bulan April</p>
+                                            <!--p class="text-muted fw-medium font-size-12">{{ $percentageOrder }}</p-->
                                         </div>
 
                                         <div class="flex-shrink-0 align-self-center">
@@ -57,9 +88,10 @@
                                 <div class="card-body" style="background-color:#dbebfa">
                                     <div class="d-flex">
                                         <div class="flex-grow-1">
-                                            <h4 class="mb-2 font-size-20">{{ $product }}</h4>
+                                            <h1 style="font-size:26px">{{ $product }}</h1>
                                             <p class="text-muted mb-0 text-truncate"><b>Produk Dikerjakan</b></p>
-                                            <p class="text-muted fw-medium font-size-12">{{ $percentageProduct }}</p>
+                                            <p class="text-muted"><span class="text-success me-2"> 50% <i class="mdi mdi-arrow-up"></i> </span> dari Bulan April</p>
+                                            <!--p class="text-muted fw-medium font-size-12">{{ $percentageProduct }}</p-->
                                         </div>
 
                                         <div class="flex-shrink-0 align-self-center">
@@ -76,9 +108,10 @@
                                 <div class="card-body" style="background-color:#dbebfa">
                                     <div class="d-flex">
                                         <div class="flex-grow-1">
-                                            <h4 class="mb-2 font-size-20">Rp {{ number_format($income, 2) }}</h4>
+                                            <h1 style="font-size:26px">Rp {{ number_format($income, 2) }}</h1>
                                             <p class="text-muted mb-0 text-truncate"><b>Pendapatan Kotor</b></p>
-                                            <p class="text-muted fw-medium font-size-12">{{ $percentageIncome }}</p>
+                                            <p class="text-muted"><span class="text-success me-2"> 70% <i class="mdi mdi-arrow-up"></i> </span> dari Bulan April</p>
+                                            <!--p class="text-muted fw-medium font-size-12">{{ $percentageIncome }}</p>-->
                                         </div>
 
                                         <div class="flex-shrink-0 align-self-center">
@@ -99,6 +132,31 @@
                         <div class="row">
                             <div class="col-4">
                                 <div class="card-body" style="background-color:#dbebfa">
+                                  <div class="mb-4 card-title">Produk Terlaris</div>
+                                  <div class="table-responsive mt-4">
+                                    <table class="table align-middle table-nowrap">
+                                      <tbody>
+                                          @foreach ($bestSellerProduct as $key => $item)
+                                          <tr>
+                                              <td style="width: 30%;">
+                                                <p class="mb-0">{{ $item['item'] }}</p>
+                                              </td>
+                                              <td style="width: 25%;">
+                                                <h5 class="mb-0">{{ $item['total_qty'] }}</h5>
+                                              </td>
+                                              <td>
+                                                <div class="bg-transparent progress-sm progress">
+                                                  <div class="progress-bar bg-success" style="width: 94%;" role="progressbar" aria-valuenow="94" aria-valuemin="0" aria-valuemax="100"></div>
+                                                </div>
+                                              </td>
+                                          </tr>
+                                          @endforeach
+                                      </tbody>
+                                    </table>
+                                  </div>
+                                </div>
+
+                                <!--div class="card-body" style="background-color:#dbebfa">
                                     <div style="display: flex; align-items: center;">
                                         <p class="text-muted mb-0 text-truncate"><b>Produk Terlaris</b></p>
                                         <div class="mini-stat-icon avatar-sm rounded-circle bg-primary"
@@ -127,38 +185,29 @@
                                             @endforeach
                                         </tbody>
                                     </table>
-                                </div>
+                                </div-->
                             </div>
                             <div class="col-8">
                                 <div class="card-body" style="background-color:#dbebfa">
                                     <div style="display: flex; align-items: center;">
-                                        <p class="text-muted mb-0 text-truncate"><b>Produk Terlaris</b></p>
-                                        <div class="mini-stat-icon avatar-sm rounded-circle bg-primary"
-                                            style="width: 24px; height: 24px; margin-left: 8px;">
-                                            <span class="avatar-title" style="padding: 0;">
-                                                <i class="bx bx-user-pin" style="font-size: 16px;"></i>
-                                            </span>
-                                        </div>
+                                        <div class="mb-4 card-title">Sales Report</div>
                                     </div>
-
-                                    <br>
-
                                     <table class="table mb-0" style="width: 100%;">
                                         <thead>
-                                            <th>#</th>
-                                            <th>Name Sales</th>
-                                            <th>Jumlah Order</th>
-                                            <th>Jumlah Produk</th>
-                                            <th>Total Pendapatan</th>
+                                            <th align="center">#</th>
+                                            <th align="center">Name Sales</th>
+                                            <th align="center">Jumlah Order</th>
+                                            <th align="center">Jumlah Produk</th>
+                                            <th align="center">Total Pendapatan</th>
                                         </thead>
                                         <tbody>
                                             @foreach ($salesReport as $key => $item)
                                                 <tr>
-                                                    <td>{{ $key + 1 }}</td>
+                                                    <td align="center">{{ $key + 1 }}</td>
                                                     <td>{{ $item->name }}</td>
-                                                    <td>{{ $item->total_order }}</td>
-                                                    <td>{{ $item->total_qty }}</td>
-                                                    <td>Rp {{ number_format($item->total_pendapatan, 2) }}</td>
+                                                    <td align="center">{{ $item->total_order }}</td>
+                                                    <td align="center">{{ $item->total_qty }}</td>
+                                                    <td align="center">Rp {{ number_format($item->total_pendapatan, 2) }}</td>
                                                 </tr>
                                             @endforeach
                                         </tbody>
